07a56be2f3fcedb728e921439557ac982c07f9ce
Rayhan-khan-Ridoy <78033774+Rayhan-khan-Ridoy@users.noreply.github.com>
2021-07-18 00:12:13 +0600

    final term assignment

---
+++ 4_tier_assignment/DAL/DEPARTMENT_REPO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAL
+{
+    public class DEPARTMENT_REPO
+    {
+         static SMSEntities context;
+         static DEPARTMENT_REPO()
+        {
+
+            context = new SMSEntities();
+        }
+        public static List<string> GetDEPARTMENTNAMES()
+        {
+            var data = context.DEPARTMENTs.Select(e => e.NAME).ToList();
+            return data;
+        }
+
+        public static List<DEPARTMENT> GetDEPARTMENT()
+        {
+            return context.DEPARTMENTs.ToList();
+        }
+        public static void AddDEPARTMENT(DEPARTMENT d)
+        {
+            context.DEPARTMENTs.Add(d);
+            context.SaveChanges();
+        }
+    }
+}
