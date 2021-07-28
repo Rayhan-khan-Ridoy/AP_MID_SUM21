9467d13a706b6366ab25010c49a208ce42abdcb0
Rayhan-khan-Ridoy <78033774+Rayhan-khan-Ridoy@users.noreply.github.com>
2021-07-28 10:40:37 +0600

    newly added students with existing departments

---
+++ new-4-tier/4_tier_assignment/Controllers/DEPARTMENTController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL;
+using BEL;
+
+namespace _4_tier_assignment.Controllers
+{
+    public class DEPARTMENTController : ApiController
+    {
+        
+
+        [Route("api/DEPARTMENT/NAMES")]
+        [HttpGet]
+
+        public List<string> GetNAMES()
+        {
+            return DEPARTMENT_SERVICE.GetDEPARTMENTNAMES();
+        }
+
+        [Route("api/DEPARTMENT/GetAll")]
+        [HttpGet]
+        public List<DEPARTMENTMODEL> GetAllDepartments()
+        {
+            return DEPARTMENT_SERVICE.GetDEPARTMENT();
+        }
+
+
+        [Route("api/DEPARTMENT/Add")]
+        [HttpPost]
+        public void Add(DEPARTMENTMODEL dept)
+        {
+          
+          DEPARTMENT_SERVICE.AddDEPARTMENT(dept);
+        }
+    }
+}
---
+++ new-4-tier/BLL/DEPARTMENT_SERVICE.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using BEL;
+namespace BLL
+{
+    public class DEPARTMENT_SERVICE
+    {
+      
+
+        public static List<string> GetDEPARTMENTNAMES()
+        {
+            return DEPARTMENT_REPO.GetDEPARTMENTNAMES();
+        }
+        public static List<DEPARTMENTMODEL> GetDEPARTMENT()
+        {
+            var Departments = DEPARTMENT_REPO.GetDEPARTMENT();
+
+            List<DEPARTMENTMODEL> data = new List<DEPARTMENTMODEL>();
+
+            foreach (var d in Departments)
+            {
+                var dm = new DEPARTMENTMODEL()
+                {
+                    ID = d.ID,
+                    NAME = d.NAME
+                };
+                data.Add(dm);
+            }
+            return data;
+        }
+
+        public static void AddDEPARTMENT(DEPARTMENTMODEL dept)
+        {
+            var d = new DEPARTMENT()
+            {
+                ID = dept.ID,
+                NAME = dept.NAME
+            };
+            DEPARTMENT_REPO.AddDEPARTMENT(d);
+        }
+    }
+}
